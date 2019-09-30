6bec1bb215c8118e967be247377889a839d14a1f
Jorge Silva <34870599+jcsilvapt@users.noreply.github.com>
2019-09-30 21:49:48 +0100

    new Update
    - Fixed pre-loader position, wasn't showing as it should be on mobile devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,3 @@
-/* 
- _____   _           _         _                        _                  
-|_   _| | |         | |       | |                      | |                 
-  | |   | |__   __ _| |_ ___  | |_ ___  _ __ ___   __ _| |_ ___   ___  ___ 
-  | |   | '_ \ / _` | __/ _ \ | __/ _ \| '_ ` _ \ / _` | __/ _ \ / _ \/ __|
- _| |_  | | | | (_| | ||  __/ | || (_) | | | | | | (_| | || (_) |  __/\__ \
- \___/  |_| |_|\__,_|\__\___|  \__\___/|_| |_| |_|\__,_|\__\___/ \___||___/
-
-Oh nice, welcome to the js file of dreams.
-Enjoy responsibly!
-@ihatetomatoes
-
-*/
-
 $(document).ready(function() {
 	
 	setTimeout(function(){
@@ -19,4 +5,37 @@ $(document).ready(function() {
 		$('h1').css('color','#222222');
 	}, 3000);
 	
+});
+
+document.ontouchstart = function(){ document.getElementById('wp').play(); }
+var navActive = false;
+var aHeight = 100;
+
+$('.navbar-effect').css({height:"100px"});
+$(window).scroll(function () {
+	if ($(document).scrollTop() > 100) {
+		if(!navActive) {
+			$('.navbar-effect').css({height: "60px"});
+		}
+		aHeight = 60;
+	} else {
+		if (!navActive) {
+			$('.navbar-effect').css({height: "100px"});
+		}
+		aHeight = 100;
+	}
+});
+
+$(document).ready(function () {
+	$('[data-toggle="tooltip"]').tooltip();
+
+});
+$(document).on('click', '.navbar-toggler', function () {
+	if(!navActive) {
+		navActive = true;
+		$('.navbar-effect').css({height: "initial"});
+	} else {
+		navActive = false;
+		$('.navbar-effect').css({height: aHeight+"px"});
+	}
 });
